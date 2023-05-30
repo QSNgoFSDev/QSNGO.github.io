e19868f2bf206ab9174cab19f17a145b01013e3d
QSngo <QSngofsdev@gmail.com>
2023-05-30 12:34:24 +0700

    add file

---
+++ code-201/lab201_1/index.js
@@ -0,0 +1,11 @@
+let myName = prompt('please fill in your name')
+
+let age = prompt ('how old are you')
+
+let gender = prompt ('what is your gender')
+
+let address = prompt ('Where do you live')
+
+alert ('Thank you ' + myName + ',' + 'you are'+ age +' years old , you are ' + gender + ', whos live in ,' + address)
+
+document.write ('Thank you ' + myName + ',' + 'you are'+ age +' years old , you are ' + gender + ', whos live in ,' + address)
